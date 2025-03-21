9e252d05af141761c0d6fa559bf39c0b161e1d1b
culturewide <eatfanbrother@gmail.com>
2025-03-21 17:38:27 +0800

    first commit

---
+++ src/dotwice.java
@@ -0,0 +1,6 @@
+public class dotwice {
+    public static int dotwice(functionhouse f, int x){
+        return f.apply(f.apply(x));
+    }
+
+}
---
+++ src/functionhouse.java
@@ -0,0 +1,3 @@
+public interface functionhouse {
+    public int apply(int x);
+}
